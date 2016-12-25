a65f55dbbaae9169617c8eb773c77cb44dbc8308
americast <sayan.sinha@iitkgp.ac.in>
2016-12-25 19:39:45 +0530

    New followers

--- social-media/github/github_helper.rb
+++ social-media/github/github_helper.rb
@@ -1,12 +1,39 @@
 require 'json'
 require 'octokit'
+require 'io/console'
 
 def github_login()
-  
-  credentials = JSON.parse(File.read("#{Dir.pwd}/secrets/github.json"))
+  begin
+        credentials=[]; count=0
+        File.open('secrets/github.txt', 'r') do |f1|  
+        while line = f1.gets
+        credentials[count]=line
+        count+=1
+        end
+        end
+  rescue
+        credentials=[]
+        print "Enter username: "
+        credentials[0]=gets
+        print "Enter password: "
+        credentials[1]=STDIN.noecho(&:gets)
+        print "\n\nWould you like to save username and password?"
+        choice=gets
+        if (choice[0]=="y"|| choice[0]=="Y")
+        count=0
+        require 'fileutils'
+        FileUtils::mkdir_p 'secrets'
+        File.open('secrets/github.txt', 'w') do |f|  
+        while count<=1
+          f.puts(credentials[count])
+          count+=1
+        end
+        end
+        end
+  end
   Octokit.configure do |c|
-    c.login = credentials[0]["Username"]
-    c.password = credentials[0]["Password"]
+    c.login = credentials[0][0,(credentials[0].length)-1]
+    c.password = credentials[1][0,(credentials[1].length)-1]
   end
 
   return Octokit
--- social-media/github/main.rb
+++ social-media/github/main.rb
@@ -1,8 +1,46 @@
 require_relative 'github_helper'
 
 def github()
-  github_client = github_login()
-  puts "\nGithub Notifier\n"
-  puts "\nSigned in, as #{github_client.user.name}\n"
-  github_client.auto_paginate = true
+  while (true)
+  begin
+    github_client = github_login()
+    puts "\nGithub Notifier\n"
+    puts "\nSigned in, as #{github_client.user.name}.\n"
+    github_client.auto_paginate = true
+    break
+  rescue
+    puts "Authentication failure. Please try again..."
+    require 'fileutils'
+    begin
+      FileUtils.remove_dir("secrets")
+    rescue
+      print ""
+    end
+  end
+  end
+tot=github_client.user[:followers]
+flist=[]; count=0
+File.open('data/github.txt', 'r') do |f1|  
+while line = f1.gets
+flist[count]=line
+count+=1
+end
+end
+
+count=0
+puts "\nNew followers:"
+myfile=File.new('data/github.txt',"w")
+while(count<tot)
+  begin
+    usernow=github_client.followers(github_client.user[:login])[count][:login]
+    if (flist.include?usernow+"\n")
+    else
+      puts usernow
+    end
+    myfile.puts(usernow)
+  rescue
+    break
+  end
+  count+=1
+end
 end
